c95fc05df1d08b4e9daa8405fcf005b6d06610eb
Jacob Lundqvist <jacob.lundqvist@gmail.com>
2019-12-11 19:26:36 +0100

    Update stop_CreativeCloud.sh
    Updated to handle latest Creative Cloud

--- stop_CreativeCloud.sh
+++ stop_CreativeCloud.sh
@@ -2,18 +2,19 @@
 #
 #  License: GPL
 #  Copyright: Jacob.Lundqvist@gmail.com
-#  Version: 1.0  2019-12-02
+#  Version: 1.1  2019-12-11 - Handling Creative Cloud as of this date
 #
-#  To get rid of Creative cloud on the Mac, several processes needs to be killed, and in a specific order...
+#  To get rid of the Creative cloud background processes on a Mac, several processes needs to be killed, 
+#  and in a specific order...
 #
-#  Thie is developed on Bash 3, using a dictionary like array, might need some changes for Bash 4...
+#  This is developed on Bash 3, using a dictionary like array, might need some changes for Bash 4...
 #
 
 echo
 echo "========================================"
 echo
 
-echo -n "We need sudo privs for this, if asked for Passwiord, enter yours ... "
+echo -n "We need sudo privs to kill some of the procs ... "
 sudo ls > /dev/null
 echo "ok!"
 echo
@@ -24,32 +25,33 @@ echo
 #
 # This is Bash 3 notation, should hopefully also work for Bash 4
 # Array pretending to be a Pythonic dictionary
-CREATIVE_CLOUD_APPS=(
 
-    "CCXProcess:Adobe_CCXProcess.node /Applications/Utilities/Adobe Creative Cloud Experience/CCXProcess/CCXProcess.app"
+
+CREATIVE_CLOUD_APPS=(
 
     "Creative Cloud:Creative Cloud.app/Contents/MacOS/Creative"
 
     "Adobe Desktop Service:Adobe Desktop Service.app/Contents/MacOS"
 
+    "CCLibrary.app:Adobe/Creative Cloud Libraries/CCLibrary.app/Contents/MacOS"
+
+    "Core Sync:Core Sync.app/Contents/MacOS"
+
+    "Adobe_CCXProcess.node:Adobe_CCXProcess.node /Applications/Utilities/Adobe Creative Cloud Experience/CCXProcess/CCXProcess.app"
+
     "com.adobe.acc.installer.v2:com.adobe.acc.installer.v2"
 
     "AdobeCRDaemon:AdobeCRDaemon"
 
-    "Core Sync:Core Sync.app/Contents/MacOS"
-
-    "CCLibrary:Adobe/Creative Cloud Libraries/CCLibrary.app/Contents/MacOS"
-
-    "AdobeIPCBroker:AdobeIPCBroker"
 )
 
 
 for app in "${CREATIVE_CLOUD_APPS[@]}" ; do
     procName="${app%%:*}"
     procPattern="${app##*:}"
-    
+
     echo "Analysing: $procName"
-    processIds=`ps ax | grep "$procPattern" | grep -v grep | awk '{print $1}'` 
+    processIds=`ps ax | grep "$procPattern" | grep -v grep | awk '{print $1}'`
     if [ $? -eq "0" ]; then
 	for procId in $processIds
 	do
